1fa6fda81056ee3826a3ff4596652d0391cc3ba4
Nikita Lyakhovoy <lyakhovoynikita@gmail.com>
2023-03-03 17:01:38 +0000

    Highlight junctions along the way

--- optimised_generation.py
+++ optimised_generation.py
@@ -1,22 +1,19 @@
 import json
 import random
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.ensemble import RandomForestRegressor
 
 class ScenarioGenerator:
-    def __init__(self, weather, road, vehicle, traffic, emergency, timeOfDay, location, num_cars, intersection, pedestrians, pedestrian_cross, num_scenarios):
+    def __init__(self, weather, vehicle, traffic, emergency, timeOfDay, location, intersections, pedestrians, pedestrian_cross,route_length, num_scenarios):
         self.weather = weather
-        self.road = road
         self.vehicle = vehicle
         self.traffic = traffic
         self.emergency = emergency
         self.timeOfDay = timeOfDay
         self.location = location
-        self.num_cars = num_cars
-        self.intersection = intersection
+        self.intersections = intersections
         self.pedestrians = pedestrians
         self.pedestrian_cross = pedestrian_cross
         self.num_scenarios = num_scenarios
+        self.route_length = route_length
 
     def generate_scenarios(self):
         generated_scenarios = []
@@ -24,16 +21,16 @@ class ScenarioGenerator:
         for i in range(self.num_scenarios):
             scenario = {
                 'weather': random.choice(self.weather),
-                'road': random.choice(self.road),
                 'vehicle': random.choice(self.vehicle),
                 'traffic': random.choice(self.traffic),
                 'emergency': random.choice(self.emergency),
                 'timeOfDay': random.choice(self.timeOfDay),
                 'location': random.choice(self.location),
-                "num_cars": random.choice(self.num_cars),
-                "intersection": random.choice(self.intersection),
+                "intersections": random.choice(self.intersections),
                 "pedestrians": random.choice(self.pedestrians),
                 "pedestrian_cross": random.choice(self.pedestrian_cross),
+                "route_length": random.choice(self.route_length),
+
             }
             generated_scenarios.append(scenario)
 
@@ -42,52 +39,19 @@ class ScenarioGenerator:
         
         return generated_scenarios
 
-
-# Define the lists of possible values for each input variable
 weather = ['Sunny', 'Rain', 'Thunderstorm']
-road = ['Highway', 'City', 'Country']
-vehicle = ['Car', 'Truck']
-traffic = ['Heavy', 'Light']
+vehicle = ['Small', 'Truck','Van']
+traffic = ['Heavy', 'Light','Medium']
 emergency = ['Yes', 'No']
-timeOfDay = ['Day', 'Night']
-location = ['Urban', 'Rural']
-num_cars = [5,10,15,2,1]
-intersection = [True, False]
+timeOfDay = ['Day', 'Night','Dawn','Dusk']
+location = ['Urban', 'Country','Downtown']
+intersections = [0,1,2,3,4,5]
 pedestrians = [True,False]
 pedestrian_cross = [True, False]
-
-# Define the number of scenarios to generate
+route_length = [50,100,120,200,250,300,400,500,600]
 num_scenarios = 100
 
-# Generate the scenarios using the ScenarioGenerator class
-scenario_generator = ScenarioGenerator(weather, road, vehicle, traffic, emergency, timeOfDay, location, num_cars, intersection, pedestrians, pedestrian_cross, num_scenarios)
+
+scenario_generator = ScenarioGenerator(weather, vehicle, traffic, emergency, timeOfDay, location, intersections, pedestrians, pedestrian_cross,route_length, num_scenarios)
 scenarios = scenario_generator.generate_scenarios()
 
-# Convert the scenarios into feature vectors (X) and target values (Y)
-X = []
-Y = []
-for scenario in scenarios:
-    x = [
-        weather.index(scenario['weather']),
-        road.index(scenario['road']),
-        vehicle.index(scenario['vehicle']),
-        traffic.index(scenario['traffic']),
-        emergency.index(scenario['emergency']),
-        timeOfDay.index(scenario['timeOfDay']),
-        location.index(scenario['location']),
-        num_cars.index(scenario['num_cars']),
-        intersection.index(scenario['intersection']),
-        pedestrians.index(scenario['pedestrians']),
-        pedestrian_cross.index(scenario['pedestrian_cross'])
-    ]
-    X.append(x)
-    Y.append([
-        scenario['num_cars'],
-        scenario['intersection'],
-        scenario['pedestrians'],
-        scenario['pedestrian_cross']
-    ])
-
-# Train a MultiOutputRegressor using a RandomForestRegressor estimator
-regressor = MultiOutputRegressor(RandomForestRegressor(n_estimators=100))
-regressor.fit(X, Y)
--- route_generator.py
+++ route_generator.py
@@ -16,41 +16,37 @@ for scenario_num in range(len(scenario_data)):
     try:
         print("new scenario: "+ str(scenario_num))
         weather = scenario_data[scenario_num]["weather"]
-        intersection = scenario_data[scenario_num]["intersection"]
-        num_cars = scenario_data[scenario_num]["num_cars"]
-        road = scenario_data[scenario_num]["road"]
+        intersections = scenario_data[scenario_num]["intersections"]
         vehicle = scenario_data[scenario_num]["vehicle"]
         traffic = scenario_data[scenario_num]["traffic"]
         emergency = scenario_data[scenario_num]["emergency"]
         timeOfDay = scenario_data[scenario_num]["timeOfDay"]
         location = scenario_data[scenario_num]["location"]
-        intersection = scenario_data[scenario_num]["intersection"]
         pedestrians = scenario_data[scenario_num]["pedestrians"]
         pedestrian_cross = scenario_data[scenario_num]["pedestrian_cross"]
+        route_length = scenario_data[scenario_num]["route_length"]
 
         # Print the values of the variables
 
         print("Weather: ", weather)
-        print("Intersection: ", intersection)
-        print("Number of cars: ", num_cars)
-        print("Road: ", road)
+        print("Intersections: ", intersections)
         print("Vehicle: ", vehicle)
         print("Traffic: ", traffic)
         print("Emergency: ", emergency)
         print("Time: ", timeOfDay)
         print("Location: ", location)
-        print("Intersection: ", intersection)
         print("Pedestrians: ", pedestrians)
         print("Pedestrian cross: ", pedestrian_cross)
+        print("Route length: ", route_length)
 
         client = carla.Client("localhost", 2000)
 
-        if location == "Rural":
-            world = client.load_world('Town07')
+        if location == "Downtown":
+            world = client.load_world('Town05')
         if location == "Urban":
             world = client.load_world('Town03')
         if location == "Country":
-            world = client.load_world('Town02')
+            world = client.load_world('Town07')
 
 
         map = world.get_map()
@@ -81,6 +77,10 @@ for scenario_num in range(len(scenario_data)):
             sun_altitude_angle=70.0
         elif timeOfDay == "Night":
             sun_altitude_angle=-30.0
+        elif timeOfDay == "Dawn":
+            sun_altitude_angle=20.0
+        elif timeOfDay == "Dusk":
+            sun_altitude_angle=110.0
 
 
 
@@ -104,46 +104,75 @@ for scenario_num in range(len(scenario_data)):
 
         spawn_points = map.get_spawn_points()
 
-        # # Set the desired number of junctions
-        # num_junctions = 3
+        # Set the desired number of junctions
+        num_junctions = intersections
 
-        # # Keep generating routes until we find one that goes through the desired number of junctions
-        # while True:
-        #     start_pose = random.choice(spawn_points)
-        #     end_pose = random.choice(spawn_points)
+        # Keep generating routes until we find one that goes through the desired number of junctions
+        while True:
+            start_pose = random.choice(spawn_points)
+            end_pose = random.choice(spawn_points)
 
-        #     waypoints = grp.trace_route(start_pose.location, end_pose.location)
+            waypoints = grp.trace_route(start_pose.location, end_pose.location)
 
-        #     # Count the number of junctions along the route
-        #     junction_count = 0
-        #     for waypoint in waypoints:
-        #         if waypoint[0].is_junction:
-        #             junction_count += 1
+            # Count the number of junctions along the route
+            junction_count = 0
+            for waypoint in waypoints:
+                if waypoint[0].is_junction:
+                    junction_count += 1
 
-        #     # Check if we found a route that goes through the desired number of junctions
-        #     if junction_count == num_junctions:
-        #         break
+            # Check if we found a route that goes through the desired number of junctions
+            if junction_count == num_junctions:
+                break
 
 
 
         waypoints = map.generate_waypoints(2.0)
 
+        def draw_waypoints(waypoints, road_id=None, life_time=50.0):
+            spawned = False 
+            for waypoint in waypoints:
+                
+                if(waypoint.road_id == road_id):
+                    world.debug.draw_string(waypoint.transform.location, "JUNC", draw_shadow=False,
+                                            color=carla.Color(r=0, g=255, b=0), life_time=life_time,
+                                            persistent_lines=True)
 
         def get_route_length(route):
             length = 0
             for i in range(len(route) - 1):
                 length += route[i][0].transform.location.distance(route[i + 1][0].transform.location)
             return length
-
+        
+        print(f"Need to find: {route_length}")
+        print(f"Need intersections: {intersections}")
         for start_waypoint in waypoints:
             for end_waypoint in waypoints:
                 # print(start_waypoint.transform.location.distance(end_waypoint.transform.location))
                 route = grp.trace_route(start_waypoint.transform.location, end_waypoint.transform.location)
-                route_length = get_route_length(route)
-                if route_length > 200 and route_length < 300:
+                found_route_length = get_route_length(route)
+              
+                # junction_count = 0
+                # for waypoint in route:
+                #     if waypoint[0].is_junction:
+                #         junction_count += 1
+
+                if found_route_length > (route_length-(route_length*0.2)) and found_route_length < (route_length+(route_length*0.2)):
                     found = True
+                    # Get unique road IDs
+                    road_ids = list(set(waypoint[0].road_id for waypoint in route))
+                    junction_road_ids = []
+                    for road_id in road_ids:
+                        # waypoints = map.generate_waypoints(2.0)
+                        for waypoint in route:
+                            if waypoint[0].road_id == road_id and waypoint[0].is_junction:
+                                junction_road_ids.append(road_id)
+                                draw_waypoints(waypoints, road_id=road_id, life_time=300)
+
+                    junctions_on_route = len(list(set(junction_road_ids)))
+                    print(f"Num of junctions: {junctions_on_route}")
+                    print(f"Unique road IDs along the route: {road_ids}")
                     print(f"Distance between: {start_waypoint.transform.location.distance(end_waypoint.transform.location)}")
-                    print(f"Route length: {route_length}")
+                    print(f"Route length: {found_route_length}")
                     break
                     # build path using the route
             if found:
@@ -174,10 +203,46 @@ for scenario_num in range(len(scenario_data)):
 
         # Spawn a vehicle and set it to drive to destination
         blueprint_library = world.get_blueprint_library()
-        vehicle_bp = blueprint_library.filter("model3")[0]
+        if vehicle == "Small":
+            vehicle_bp = blueprint_library.filter("a2")[0]
+        elif vehicle == "Truck":
+            vehicle_bp = blueprint_library.filter("cybertruck")[0]
+        elif vehicle == "Van":
+             vehicle_bp = blueprint_library.filter("carlacola")[0]
+            
+        
         vehicle_actor = world.spawn_actor(vehicle_bp, carla.Transform(first_location+carla.Location(z=0.5)))
         vehicles_list = []
         vehicles_list.append(vehicle_actor)
+
+        for vehicle in vehicles_list:
+
+                
+            vehicle_physics_control = vehicle.get_physics_control()
+
+            if weather == "Rain":
+                # Create Wheels Physics Control
+                    
+                front_left_wheel = carla.WheelPhysicsControl(tire_friction=0.7,max_steer_angle=70)
+                front_right_wheel = carla.WheelPhysicsControl(tire_friction=0.7,max_steer_angle=70)
+                rear_left_wheel = carla.WheelPhysicsControl(tire_friction=0.7,max_steer_angle=0)
+                rear_right_wheel = carla.WheelPhysicsControl(tire_friction=0.7,max_steer_angle=0)
+                wheels = [front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel]
+                vehicle_physics_control.wheels = wheels 
+                vehicle.apply_physics_control(vehicle_physics_control)
+
+                print("Changed grip to Rain")
+
+            if weather == "Thunderstorm":
+                front_left_wheel = carla.WheelPhysicsControl(tire_friction=0.3,max_steer_angle=70)
+                front_right_wheel = carla.WheelPhysicsControl(tire_friction=0.3,max_steer_angle=70)
+                rear_left_wheel = carla.WheelPhysicsControl(tire_friction=0.3,max_steer_angle=0)
+                rear_right_wheel = carla.WheelPhysicsControl(tire_friction=0.3,max_steer_angle=0)
+                wheels = [front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel]
+                vehicle_physics_control.wheels = wheels 
+                vehicle.apply_physics_control(vehicle_physics_control)
+                print("Changed grip Thunder")
+
 
         agent = BasicAgent(vehicle_actor)
 
